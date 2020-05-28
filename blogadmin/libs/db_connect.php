cebfaae42c65ef1fbef823eb35a7e36c05711523
Luis Ramos <iepvlramos@gmail.com>
2020-05-28 12:47:46 -0500

    version1.0

---
+++ blogadmin/libs/db_connect.php
@@ -0,0 +1,8 @@
+<?php
+$con=mysqli_connect("localhost","root","2012116664","blog_admin_db");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+ ?>
---
+++ database/conn.php
@@ -0,0 +1,4 @@
+<?php
+//database connection
+($GLOBALS["___mysqli_ston"] = mysqli_connect("localhost","root","2012116664","blog_admin_db"));  //host,user,password,database
+?>
